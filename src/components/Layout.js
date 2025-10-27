743237d72ca37e26cd496228e621426c3479f57f
Jannick <jannickjensen1@hotmail.com>
2025-10-27 12:30:40 +0100

    added link to home on header logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="w-full bg-white py-4">
+        <Link to="/">
         <div className="container mx-auto flex justify-center">
           <img 
             src="/images/feriepartner-logo.png" 
@@ -12,6 +14,7 @@ function Layout() {
             className="h-16 object-contain"
           />
         </div>
+        </Link>
       </header>
       <main className="flex-1">
         <Outlet />
