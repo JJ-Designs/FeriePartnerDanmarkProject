a58553bc74238713bc82c9a3fd206685430b8dfb
Jannick <jannickjensen1@hotmail.com>
2025-10-27 15:48:17 +0100

    added header component with simple navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,14 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import DevSettings, { DEV_CONFIG } from './DevSettings';
+import Header from './Header';
 
 function Layout() {
   const [devSettings, setDevSettings] = useState(DEV_CONFIG);
 
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="w-full bg-white py-4">
-        <Link to="/">
-        <div className="container mx-auto flex justify-center">
-          <img 
-            src="/images/feriepartner-logo.png" 
-            alt="Feriepartner Logo" 
-            className="h-16 object-contain"
-          />
-        </div>
-        </Link>
-      </header>
+      <Header />
       <main className="flex-1">
         <Outlet context={{ devSettings }} />
       </main>
